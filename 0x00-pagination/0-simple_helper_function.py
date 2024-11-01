948d2698079a30a45a574a736721681be8deeee4
Ebnen <Ebenzy1996@gmail.com>
2024-11-01 18:21:40 +0100

    paginization

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""pagination 101"""
+
+def index_range(page: int, page_size: int) -> tuple:
+    """return a tuple of size two containing a start index and an end index"""
+    return ((page - 1) * page_size, page * page_size)
